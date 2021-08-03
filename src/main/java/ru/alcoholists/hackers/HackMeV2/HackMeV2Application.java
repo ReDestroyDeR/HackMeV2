235f8f7c29e64113e11138094b72f29c15b95a15
Daniil Shreyder <redestroyder@gmail.com>
2021-08-03 18:01:03 +0300

    Initial Commit

---
+++ src/main/java/ru/alcoholists/hackers/HackMeV2/HackMeV2Application.java
@@ -0,0 +1,13 @@
+package ru.alcoholists.hackers.HackMeV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackMeV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackMeV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/alcoholists/hackers/HackMeV2/HackMeV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alcoholists.hackers.HackMeV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackMeV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
